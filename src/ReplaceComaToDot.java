46bddf6838ad383586ba0a511716b3d38fd214bb
shupelick <42003283+shupelick@users.noreply.github.com>
2021-10-18 14:11:15 +0300

    Update ReplaceComaToDot.java

--- src/ReplaceComaToDot.java
+++ src/ReplaceComaToDot.java
@@ -3,11 +3,20 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class ReplaceComaToDot {
     public static void main(String[] args) {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter("dot_file.txt"));
-             BufferedReader reader = new BufferedReader(new FileReader("coma_file.txt")))
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input file path: ");
+        String fileName = in.nextLine();
+        replaceComaToDot(fileName);
+    }
+
+    public static void replaceComaToDot(String inputFilePath){
+        String outputFilePath = inputFilePath.substring(0, inputFilePath.lastIndexOf("\\") + 1) + "output_file.txt";
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputFilePath));
+             BufferedWriter writer = new BufferedWriter(new FileWriter(outputFilePath)))
         {
             String line;
             while ((line = reader.readLine()) != null) {
@@ -17,5 +26,6 @@ public class ReplaceComaToDot {
         } catch (IOException e){
             e.printStackTrace();
         }
+        System.out.print("Coma is replaced by dot in file: " + outputFilePath);
     }
 }
